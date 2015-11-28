b8ed39764aa2c6466f577f587d77479e0b8b8a42
Christopher Lopez <chlo1259@gmail.com>
2015-11-28 20:00:54 +0000

    Renamed again

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Christopiphy
+# Christophify
 
 This README outlines the details of collaborating on this Ember application.
 A short introduction of this app could easily go here.
--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'christopiphy',
+    modulePrefix: 'christophify',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
