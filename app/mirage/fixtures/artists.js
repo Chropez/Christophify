2c39f5690ff903c5ea07b9d0f88afb72f4be566f
Christopher Lopez <chlo1259@gmail.com>
2015-12-01 07:16:56 +0000

    added styling artis route

--- app/mirage/fixtures/artists.js
+++ app/mirage/fixtures/artists.js
@@ -3,42 +3,42 @@ export default [
     id: 1,
     name: 'Justin Bieber',
     thumbnail: 'https://i.scdn.co/image/ca283ddea2afc65c15d802d45ee3d3fd255ab4e2',
-    coverImage: ''
+    coverImage: 'https://artistheader.scdn.co/1uNFoZAHBGtllmzznpCI3s/desktop_cat/83ae2349-5ff8-4718-8a1e-93269aec97f9'
   },
   {
     id: 2,
     name: 'One direction',
     thumbnail: 'https://i.scdn.co/image/45741ba38b51130bb96e00c0e24d17ba311a9293',
-    coverImage: ''
+    coverImage: 'https://artistheader.scdn.co/4AK6F7OLvEQ5QYCBNiQWHq/desktop_cat/175af2ea-30e6-4626-94d9-a3fb7c366a42'
   },
   {
     id: 3,
     name: 'Adele',
     thumbnail: 'https://i.scdn.co/image/df070ad127f62d682596e515ac69d5bef56e0897',
-    coverImage: ''
+    coverImage: 'https://artistheader.scdn.co/4dpARuHxo51G3z768sgnrY/desktop_cat/4e6467c0-2f56-48ee-b8e1-78cb560a8222'
   },
   {
     id: 4,
     name: 'Drake',
     thumbnail: 'https://i.scdn.co/image/94c531c2ed71ce23d704d7f63a168c0287bbbb23',
-    coverImage: ''
+    coverImage: 'https://artistheader.scdn.co/3TVXtAsR1Inumwj472S9r4/desktop_cat/33383526-14a3-438e-ad9a-be85a489220a'
   },
   {
     id: 5,
     name: 'The Weeknd',
     thumbnail: 'https://i.scdn.co/image/2b8c98cc542f350d6b7ddf77a525866ce75270d2',
-    coverImage: ''
+    coverImage: 'https://artistheader.scdn.co/1Xyo4u8uXC1ZmMpatF05PJ/desktop_cat/2dcd2836-2f3a-42c6-be87-a7ce4648fbef'
   },
   {
     id: 6,
     name: 'Taylor Swift',
     thumbnail: 'https://d3rt1990lpmkn.cloudfront.net/640/dfa177dfb6485c811852c83d846c894b8bd976a0',
-    coverImage: ''
+    coverImage: 'https://artistheader.scdn.co/06HL4z0CvFAxyc27GXpf02/desktop_cat/608db808-d970-41f5-b580-b1f3d3ee7074'
   },
   {
     id: 7,
     name: 'Ed Sheeran',
     thumbnail: 'http://profile-images.scdn.co/artists/default/436a91744e5b63843023f15477656c953c9e2b21',
-    coverImage: ''
+    coverImage: 'https://artistheader.scdn.co/6eUKZXaKkcviH0Ku9w2n3V/desktop_cat/edf24069-4caa-4fa5-b195-128ce3f82e5d'
   }
 ];
